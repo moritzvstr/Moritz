271e54465dee5ad4372e04f6af48c0f0142cd8be
moritzvstr <M.Strempel@campus.lmu.de>
2024-04-26 15:10:24 +0200

    vis cahnge

---
+++ visualization.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import csv
+
+
+#with open('results_combined.csv', 'r') as csvfile:
+#    data = csv.reader(csvfile, delimiter = ';')
+
+df_nohead = pd.read_csv('results_combined.csv', sep = ';', header = None)
+  
+# adding header 
+headerList = ['Time', 'Threads', 'seqlen', 'dbsize', 'type'] 
+  
+# converting data frame to csv 
+df_nohead.to_csv('results_combined2.csv', header=headerList, index=False) 
+
+df = pd.read_csv('results_combined2.csv') 
+
+print(df.head())
+print(df['Time'].head())
+
+
+
+
+plt.scatter(df['dbsize'], df['Time'], label = 'test')
+plt.xlabel('dbsize')
+plt.ylabel('Time')
+plt.title('test')
+plt.legend()
+plt.show()
